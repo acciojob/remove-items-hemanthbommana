812016f977ebd097b5fb32a8707fdf1bdbf05737
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-07-30 10:39:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let myFunction = (event)=>{
+	let eve = document.getElementById('colorSelect').selectedIndex ;
+	if(eve != -1){
+		document.getElementById('colorSelect').remove(eve);
+	}
+}
+let event = document.getElementsByTagName('input')[0].addEventListener('click',myFunction);
